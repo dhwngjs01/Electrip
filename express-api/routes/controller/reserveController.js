31006146b977098f872af938886b8c2e59112a76
= <dhwngjs01@naver.com>
2023-05-24 03:26:24 +0900

    ADD - 예약하기 API
    1. 차량 목록 LOAD (GET)
    [장소 번호, 대여 시작 날짜, 대여 종료 날짜]
    차량 목록들을 불러올 때 예약되지 않는 차량들만 가져오는 SQL문 수정
    
    2. 예약하기 (POST)
    [사용자 번호, 차량 번호, 총 금액, 시작 장소 번호, 대여 시작 날짜, 대여 종료 날짜]
    예약 테이블에 추가

--- express-api/routes/controller/reserveController.js
+++ express-api/routes/controller/reserveController.js
@@ -21,9 +21,50 @@ exports.getCarListFromZone = async (req, res) => {
 
   // 장소에 해당하고 날짜 범위 안에 포함하지 않는 차량 가져오기
   const result = await db.query(
-    "select * from car where zone_no = $1 and car_no not in (select car_no from reserve where reserve_start_date < $2 and reserve_end_date > $3) and car_active = true",
-    [zoneNo, endDate, startDate]
+    `SELECT * 
+    from car 
+    where zone_no = $1 AND 
+    car_no not IN 
+      (
+        select car_no from reserve WHERE 
+        (reserve_start_date BETWEEN $2 AND $3) OR 
+        (reserve_end_date BETWEEN $2 and $3) OR 
+        ($2 BETWEEN reserve_start_date AND reserve_end_date) OR 
+        ($3 BETWEEN reserve_start_date AND reserve_end_date)
+      ) AND 
+    car_active = true`,
+    [zoneNo, startDate, endDate]
   );
 
   res.status(200).json(result.rows);
+};
+
+exports.reserve = async (req, res) => {
+  let user_no = req.body.user_no;
+  let car_no = req.body.car_no;
+  let reserve_total_price = req.body.reserve_total_price;
+  let reserve_start_zone_no = req.body.reserve_start_zone_no;
+  let reserve_start_date = req.body.reserve_start_date;
+  let reserve_end_date = req.body.reserve_end_date;
+
+  // 예약하기
+  const result = await db.query(
+    `INSERT INTO reserve
+    (user_no, car_no, reserve_total_price, reserve_start_zone_no, reserve_start_date, reserve_end_date)
+    VALUES ($1, $2, $3, $4, $5, $6)`,
+    [
+      user_no,
+      car_no,
+      reserve_total_price,
+      reserve_start_zone_no,
+      reserve_start_date,
+      reserve_end_date,
+    ]
+  );
+
+  if (result.rowCount == 1) {
+    res.status(200).json({ message: "예약이 완료되었습니다." });
+  } else {
+    res.status(500).json({ message: "예약에 실패했습니다." });
+  }
 };
--- express-api/routes/urls/reserve.js
+++ express-api/routes/urls/reserve.js
@@ -5,7 +5,8 @@ const controller = require("../controller/reserveController");
 
 router.get("/zone", controller.getZoneList);
 router.get("/zone/:zoneNo/carList", controller.getCarListFromZone);
-
 router.get("/car", controller.getAllCarList);
+
+router.post("/", controller.reserve);
 
 module.exports = router;
