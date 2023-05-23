dca6414b267c6e1fa0136f6bd3efdb0129f20363
= <dhwngjs01@naver.com>
2023-05-24 03:32:12 +0900

    Update loading.js

--- nextjs/app/public/reserve/loading.js
+++ nextjs/app/public/reserve/loading.js
@@ -2,13 +2,16 @@
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Loading() {
   let session = useSession();
   let router = useRouter();
 
-  if (session.status === "unauthenticated") {
-    alert("로그인이 필요한 서비스입니다.");
-    router.push("/member/login");
-  }
+  useEffect(() => {
+    if (session.status === "unauthenticated") {
+      alert("로그인이 필요한 서비스입니다.");
+      router.push("/member/login");
+    }
+  }, [session.status]);
 }
