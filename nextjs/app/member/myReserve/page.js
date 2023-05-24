8cb1a6e7bb21dfee114a8cf107b3316f9c4c0f54
= <dhwngjs01@naver.com>
2023-05-25 07:29:06 +0900

    ADD - 예약조회 페이지

---
+++ nextjs/app/member/myReserve/page.js
@@ -0,0 +1,10 @@
+import MyReserveList from "@/components/pages/member/myReserve/MyReserveList";
+
+export default function MyReserve() {
+  return (
+    <div className="container">
+      <h2 className="text-center fw-bold mt-4">예약 내역</h2>
+      <MyReserveList />
+    </div>
+  );
+}
