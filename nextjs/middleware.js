0f6ea201f32ea5c9742fd99dada5ac18315dcef6
= <dhwngjs01@naver.com>
2023-05-31 04:36:01 +0900

    .

--- nextjs/middleware.js
+++ nextjs/middleware.js
@@ -8,6 +8,15 @@ export async function middleware(req) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
+  if (
+    req.nextUrl.pathname.startsWith("/member/myReserve") &&
+    session === null
+  ) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+  } else {
+    // 데이터베이스에서 나의 예약 목록을 가져온다.
+  }
+
   if (req.nextUrl.pathname.startsWith("/public/reserve") && session === null) {
     return NextResponse.redirect(new URL("/api/auth/signin", req.url));
   }
