558265f74312b5cdea9a3e686b7c9fd8624cb9ad
= <dhwngjs01@naver.com>
2023-06-04 10:09:50 +0900

    UPDATE - 관리 관련 API 요청 URL 맵핑

--- express-api/routes/urls/admin.js
+++ express-api/routes/urls/admin.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require("../controller/adminController");
 
 router.get("/dashboard", controller.getDashboard);
+router.get("/users", controller.getAllUsers);
+router.patch("/users/:id", controller.userStateControl);
+router.get("/cars", controller.getAllCars);
+router.patch("/cars/:car_no", controller.carStateControl);
 
 module.exports = router;
